95e08ccb2fc50915dfd6aaa2297d2e06c6285513
InfoColony <piusebiama@gmail.com>
2023-06-15 15:03:53 -0700

    created 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main()
+\**
+ *The main function outputs strings to the terminal/console
+ */
+
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
