f5fca560ecd1cf81dea4ad6ad6c92e7d07a0d0e9
InfoColony <piusebiama@gmail.com>
2023-06-15 20:27:36 -0700

    created 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, quit, sizeof(quot) - 1);
+	write(2, quot, sizeof(quot) - 1);
 	return (1);
 }
