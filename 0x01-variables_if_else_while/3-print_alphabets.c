f096e291d37d265d9fc7ad3f2f7ffde7faca2d1b
InfoColony <piusebiama@gmail.com>
2023-06-16 18:47:53 -0700

    Created a 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a = 'a';
+	char A = 'A';
+	char n = '\n';
+
+	do {
+		if (a <= 'z')
+			putchar(a);
+		else
+			putchar(A);
+
+	} while (a < 'z' || A <= 'A');
+	putchar(n);
+}
