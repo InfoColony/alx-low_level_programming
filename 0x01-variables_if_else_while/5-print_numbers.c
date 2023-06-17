fc465eea2b6dd28153eca52cde9119b84c125762
InfoColony <piusebiama@gmail.com>
2023-06-16 19:35:55 -0700

    Created 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cnt = 0;
+
+	for (; cnt < 10; cnt++)
+		printf("%d\n", cnt)
+	return (0);
+}
