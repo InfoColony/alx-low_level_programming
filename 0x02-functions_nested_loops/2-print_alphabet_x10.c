61b38bfdf27d12a2cfac4a6410007f106d41b501
InfoColony <piusebiama@gmail.com>
2023-06-20 14:02:06 -0700

    created 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - main function
+ * A function to print all alphabets 10x
+ *
+ * Return: Always 0 (Successful)
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		for (; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,16 +2,3 @@
 
 
 void print_alphabet(void);
-
-/*void print_alphabet()
-{
-	char a = 'a';
-	char n = '\n';
-
-	for (; a <= 'z'; a++)
-	{
-		write(1,&a,1);
-	}
-	write(1,&n,1);
-	return;
-}*/
