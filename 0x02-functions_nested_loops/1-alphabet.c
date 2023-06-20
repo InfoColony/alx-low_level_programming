9e1a20f6f51445addbb829c37a81eb285efc52b7
InfoColony <piusebiama@gmail.com>
2023-06-20 10:19:56 -0700

    created 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *A program thhat prints all alphabets in small letter
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,16 @@
 #include <string.h>
 
 
-int _putchar();
+void print_alphabet(void);
+
+void print_alphabet()
+{
+	int a = 'a';
+
+	for (; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return;
+}
