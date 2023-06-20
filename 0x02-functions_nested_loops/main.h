647250657d52effe036ff5fc3cdd305fdf80a8c5
InfoColony <piusebiama@gmail.com>
2023-06-20 10:19:56 -0700

    created 1-alphabet.c file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,11 +6,12 @@ void print_alphabet(void);
 void print_alphabet()
 {
 	char a = 'a';
+	char n = '\n';
 
 	for (; a <= 'z'; a++)
 	{
 		write(1,&a,1);
 	}
-	write(1,'\n',1);
+	write(1,n,1);
 	return;
 }
