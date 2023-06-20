0d54f09be2edc3b922fa289c9d520e76c86918b3
InfoColony <piusebiama@gmail.com>
2023-06-20 14:02:06 -0700

    created 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - a function used to check if a character is lower case.
+ * C- the argument or parameter use i
+ * Return: it retuns the number of c.
+ */
+
+/* parameter to print c number */
+int _islower(int c)
+{
+	/** the variable for c */
+	if (c > 90)
+		return (true);
+	else
+		return (false);
+}
