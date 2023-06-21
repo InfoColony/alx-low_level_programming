f30f4fefe08888b9c573e0c99ebffd3449153660
InfoColony <piusebiama@gmail.com>
2023-06-21 14:26:38 -0700

    Created int 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - A function that adds 2 integers together
+ * @a: an argument to be added
+ * @b: an argument to be added
+ * Return: added results
+ */
+
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
