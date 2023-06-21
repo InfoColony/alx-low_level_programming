8cf31a3bdc14746a991dbd44ad9ff1d946f4d03f
InfoColony <piusebiama@gmail.com>
2023-06-21 08:42:24 -0700

    created 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			if (k < 10)
 			{
 				_putchar('0' + k);
-				if (j < 9 && 9 > ((j + 1) * i)
+				if (j < 9 && 9 > ((j + 1) * i))
 				{
 					_putchar(',');
 					_putchar(' ');
