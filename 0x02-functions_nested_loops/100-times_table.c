370d631ebc8564d506750b6803c70a5722c57148
InfoColony <piusebiama@gmail.com>
2023-06-21 16:14:07 -0700

    Created 100-times_table.c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,7 @@ void print_times_table(int n)
 							_putchar(' ');
 							_putchar(' ');
 						}
-						else if (j - 1 < n && 9 < ((j + 1) * i))
+						else if ((j - 1) < n && 9 < ((j + 1) * i))
 						{
 							_putchar(',');
                             _putchar(' ');
