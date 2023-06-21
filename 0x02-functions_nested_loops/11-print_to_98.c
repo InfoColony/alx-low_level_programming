6bcb6528f03805e7a817d4127c4aa8bb892e5782
InfoColony <piusebiama@gmail.com>
2023-06-21 14:56:42 -0700

    Created 11-print_to_98.c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - A funtion that prints natural numbers upto 98
+ * @n: The starting point of the function
+ */
+
+
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	else
+		printf("%d", n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,5 @@ int _putchar(char n);
 void times_table(void);
 
 int add(int, int);
+
+void print_to_98(int n);
