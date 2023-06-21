1d6f2b0135cdb2ea6caf2cb20d77d2adff256658
InfoColony <piusebiama@gmail.com>
2023-06-21 16:14:07 -0700

    Created 100-times_table.c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * print_times_table - A function that prints n times table
+ * print_times_table - A function that prints times table
  * @n: the number of the times table the function would print
+ * above_100 - A function to print numbers above 99
+ * @z: the input of the function above_100
  */
 
 
@@ -27,54 +29,7 @@ void print_times_table(int n)
 				j = 0;
 				for (; j <= n; j++)
 				{
-					k = i * j;
-					if (k <= 9)
-					{
-						_putchar('0' + k);
-						if (j < n && 9 >= ((j + 1) * i))
-						{
-							_putchar(',');
-							_putchar(' ');
-							_putchar(' ');
-							_putchar(' ');
-						}
-						else if (j < n && 9 < ((j + 1) * i))
-						{
-							_putchar(',');
-                            _putchar(' ');
-                            _putchar(' ');
-						}
-					}
-					else if (k > 9 && k < 100)
-					{
-						_putchar('0' + (k / 10));
-						_putchar('0' + (k % 10));
-						if (j < n)
-						{
-							if (j == n)
-								continue;
-							else if (j < n && ((j + 1) * i) > 99)
-							{
-								_putchar(',');
-								_putchar(' ');
-							}
-							else
-							{
-								_putchar(',');
-								_putchar(' ');
-								_putchar(' ');
-							}
-						}
-					}
-					else if (k > 99)
-					{
-						above_100(k);
-                        if (j < n || 9 >= ((j - 1) * i))
-                        {
-                            _putchar(',');
-                            _putchar(' ');
-						}
-					}
+					factored_code(n, i, j, k);
 				}
 				_putchar('\n');
 			}
@@ -84,8 +39,8 @@ void print_times_table(int n)
 
 
 /**
- * above_100 - A function to print numbers above 99                                                                                                  
- * @z: the input of the function above_100
+ * above_100 - A function to print numbers above 99
+ * @z: the input to function above_100
  */
 
 void above_100(int z)
@@ -99,4 +54,65 @@ void above_100(int z)
 	_putchar('0' + f);
 	_putchar('0' + s);
 	_putchar('0' + t);
+}
+
+/**
+ * factored_code - factored the code for more readability
+ * @i: an argument
+ * @j: an argument
+ * @k: an argument
+ * @n: an argument
+ */
+
+
+void factored_code(int n, int i, int j, int k)
+{
+	k = i * j;
+	if (k <= 9)
+	{
+		_putchar('0' + k);
+		if (j < n && 9 >= ((j + 1) * i))
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		else if (j < n && 9 < ((j + 1) * i))
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+	}
+	else if (k > 9 && k < 100)
+	{
+		_putchar('0' + (k / 10));
+		_putchar('0' + (k % 10));
+		if (j < n)
+		{
+			if (j == n)
+				continue;
+			else if (j < n && ((j + 1) * i) > 99)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	}
+	else if (k > 99)
+	{
+		above_100(k);
+		if (j < n || 9 >= ((j - 1) * i))
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
 }
