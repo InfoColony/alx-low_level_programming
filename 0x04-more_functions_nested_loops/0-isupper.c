84346c1d88029e88a6b4ccc747f4db8e6b94e163
InfoColony <piusebiama@gmail.com>
2023-06-22 16:14:24 -0700

    created 0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,14 @@
 /**
  * _isupper - A function that checks for uppercase character
  * @c: the character to check upon
- * Return: Always 0
+ * Return: return 0 or 1
  */
 
 
-
-
-
-
-int _isupper(int c);
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
