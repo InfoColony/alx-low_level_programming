1c050b7645dd30436cf5e66e30248b341dd9e2f6
InfoColony <piusebiama@gmail.com>
2023-06-22 16:46:46 -0700

    created 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiply's two inputs and return their result
+ * @a: first input
+ * @b: second input
+ * Return: return the multiplied result
+ */
+
+
+int mul(int a, int b)
+{
+	int r = a * b;
+
+	return (r);
+}
