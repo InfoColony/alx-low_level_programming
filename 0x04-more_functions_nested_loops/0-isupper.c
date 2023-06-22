869129ecedc82ff364c3909121e01abedad85120
InfoColony <piusebiama@gmail.com>
2023-06-22 16:14:24 -0700

    created 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - A function that checks for uppercase character
+ * @c: the character to check upon
+ * Return: Always 0
+ */
+
+
+
+
+
+
+int _isupper(int c);
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+A project at ALX on C functions and nested loops.
+A task file can be found in this directory.
