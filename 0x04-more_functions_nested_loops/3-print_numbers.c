975c60a44d2500b0c9009cceebf256097224f039
InfoColony <piusebiama@gmail.com>
2023-06-22 16:53:17 -0700

    created 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - A function that prints from 0 - 9
+ */
+
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
