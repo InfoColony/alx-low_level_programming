3e5097917bc385c92938588444769d949968ce5b
InfoColony <piusebiama@gmail.com>
2023-06-23 00:08:49 -0700

    created 10-print_triangle.c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include <main.h>
+
+
+/**
+ * print_triangle - A function that prints triangle to the terminal
+ * @size: the input of the function
+ */
+
+
+
+
+void print_triangle(int size)
+{
+	int i, r, s, h;
+
+	if (size > 0)
+	{
+		i = 1;
+		h = size;
+		for (; i <= size; i++)
+		{
+			s = 1;
+			r = h;
+			for (; r >= 2; r--)
+				_putchar(' ');
+			for (; s <= i; s++)
+				_putchar('#');
+			_putchar('\n');
+			h--;
+		}
+	} else
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+}
