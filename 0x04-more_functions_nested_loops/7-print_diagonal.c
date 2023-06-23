c9daa65d179c7a59016262e2323344b57e29042d
InfoColony <piusebiama@gmail.com>
2023-06-22 21:01:15 -0700

    created 7-print_diagonal.c file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - A function that prints a line to the terminal
+ * @n: the input to the function
+ */
+
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i = 1;
+
+		for (; i <= n; i++)
+		{
+			int j = 1;
+
+			for (; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
