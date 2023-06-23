a3c02604aa2dd88501965390b33afdd77f6b6780
InfoColony <piusebiama@gmail.com>
2023-06-22 19:57:33 -0700

    created 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - A function that prints a line to the terminal
+ * @n: the input to the function
+ */
+
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for (; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
