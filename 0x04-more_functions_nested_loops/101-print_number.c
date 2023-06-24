58cb3ddc4cf6496577b8c66ddef7c3697bbff800
InfoColony <piusebiama@gmail.com>
2023-06-23 20:09:17 -0700

    created 101-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,12 +10,14 @@
 
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int j = n;
+
+	if (j < 0)
 	{
 		_putchar('-');
-		n = -n;
+		j = -j;
 	}
-	if (n >= 10)
-		print_number(n / 10);
-	_putchar('0' + n % 10);
+	if (j >= 10)
+		print_number(j / 10);
+	_putchar('0' + j % 10);
 }
