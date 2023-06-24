4b678228037bb49d42e47de18c5172ac9032ab57
InfoColony <piusebiama@gmail.com>
2023-06-23 20:09:17 -0700

    created 101-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,21 +3,50 @@
 
 /**
  * print_number - a function that prints the given input
- * @n: the input
+ *  the input
+ * signedint - input is signed
+ * unsint - input is unsigned
+ * @int: input
  */
 
+
+void signedint(int);
+
+void unsint(unsigned int);
 
 
 void print_number(int n)
 {
-	unsigned int j = n;
+	if (n < 0)
+		signedint(n);
+	else
+		unsint(n);
+}
 
-	if (j < 0)
+/**
+ * signedint - input is signed
+ * @s: input
+ */
+void signedint(int s)
+{
+	if (s < 0)
 	{
 		_putchar('-');
-		j = -j;
+		s = -s;
 	}
-	if (j >= 10)
-		print_number(j / 10);
-	_putchar('0' + j % 10);
+	if (s >= 10)
+		signedint(s / 10);
+	_putchar('0' + s % 10);
+}
+
+/**
+ * unsint - input is unsigned
+ * @us: input
+ */
+
+void unsint(unsigned int us)
+{
+	if (us >= 10)
+		unsint(us / 10);
+	_putchar('0' + us % 10);
 }
