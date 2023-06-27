54917bed6f62f0d860c65f49999503f00309a5f6
InfoColony <piusebiama@gmail.com>
2023-06-27 12:17:48 -0700

    created 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * print_array - A function that prints all the element in an array
+ * @a: an input to the function
+ * @n: an input to the function
+ */
+
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar(a[i]);
+		if (i + 1 != n)
+		{
+			_putchar(',');
+			_putchar(' ');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
