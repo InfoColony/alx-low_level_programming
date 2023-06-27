50d100a0518c8850297a0cf287d508010f42fbb6
InfoColony <piusebiama@gmail.com>
2023-06-26 20:37:23 -0700

    created 6-puts2.c file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,12 @@ void puts2(char *str)
 {
 	char *i = str;
 	int j = 0;
+
 	while (*i != '\0')
 	{
 		if (j == 0 || j % 2 == 0)
-		{
 			_putchar(*i);
-			_putchar('\n');
-		}
+		_putchar('\n');
 		j++;
 		i++;
 	}
