04457eb245376a10635a40e3011487046626030d
InfoColony <piusebiama@gmail.com>
2023-06-27 11:12:02 -0700

    created 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * puts_half - A function that prints half of a string
+ * @str: an iput to the function
+ */
+
+void puts_half(char *str)
+{
+	char *i = str;
+	int j = 0;
+
+	while (*str != '\0')
+	{
+		j++;
+	}
+	j--;
+	j = j / 2;
+	while (j >= 0)
+	{
+		_putchar(*i);
+		j--;
+		i++;
+	}
+	_putchar('\n');
+}
