1e8efa037426df8edb971bb0bb94cf09baacfcc9
InfoColony <piusebiama@gmail.com>
2023-06-27 22:55:46 -0700

    created 100-atoi.c file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * _atoi - A function that converts string to int
+ * @s: the input of the function
+ * Return: if no int return 0 else return input int
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int m = 0;
+	int p = 0;
+	int t = 0;
+	int d = 0;
+	int s = 1;
+
+	while (*s != '\0')
+	{
+		if (*s >= 48 && *s <= 57)
+		{
+			d = *s - '0';
+			t = (t * 10) + d;
+			i++;
+		}
+		if (i > 0 && (*s < 48 || *s > 57))
+			break;
+		m = (*s == 45) ? m++ : ;
+		p = (*s == 43) ? p++ : ;
+	}
+	s = (p >= m) ? : -1;
+	if (i > 0)
+		return (t * s);
+	else
+		return (0);
+}
