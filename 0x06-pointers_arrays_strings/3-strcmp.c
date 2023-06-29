c9c1b3617c8dfee76d21c5b6b0801ab34ffeea9d
InfoColony <piusebiama@gmail.com>
2023-06-29 06:53:51 -0700

    created 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - A function that compares two string
+ * @s1: An input to the function
+ * @s2: an input to the function
+ * Return: return an int
+ */
+
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int r;
+	
+	while (*s1 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			r = *s1 - *s2;
+			break;
+		}
+		else
+			r = 0;
+		s1++;
+		s2++;
+	}
+	return(r);
+}
