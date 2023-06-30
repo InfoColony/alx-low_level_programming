b61b96cc5608c0f4740fae7bad4ac3be72a8b3a7
InfoColony <piusebiama@gmail.com>
2023-06-30 10:49:25 -0700

    created 100-rot13.c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,40 @@
+#include <main.h>
+
+
+/**
+ * rot13 - A function that creates a code 
+ * @r: an input to the function
+ * return: return the created code
+ */
+
+char *rot13(char *r)
+{
+	char *p = r;
+
+	while (*p != '\0')
+	{
+		char a = 'a';
+
+		while (((*p >= 'a' && *p <= 'z') || (*p >= 'A' && *p <= 'Z')) && a <= 'z')
+		{
+			if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+			{
+				char t = *p;
+				t = t + 13;
+				*p = t;
+				break;
+			}
+			else
+			{
+				char t1 = *p;
+				t1 = t1 - 13;
+				*p = t;
+				break;
+			}
+			a++;
+		}
+		p++;
+	}
+	
+	return (r);
+}
