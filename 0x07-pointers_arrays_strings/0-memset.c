48fd35b33957c3708c42886f07cd6e6ef75d2bbb
InfoColony <piusebiama@gmail.com>
2023-07-03 08:47:32 -0700

    created 0-memset.c and main.h files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,6 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 	char *j = s;
+	char *t = s;
 
 	while (i < n)
 	{
@@ -20,5 +21,5 @@ char *_memset(char *s, char b, unsigned int n)
 		j++;
 		i++;
 	}
-	return (s);
+	return (t);
 }
