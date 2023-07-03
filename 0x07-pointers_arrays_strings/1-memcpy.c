9187cb6413f5c3a2cb57bf572f053ee9ac4cd2a1
InfoColony <piusebiama@gmail.com>
2023-07-03 10:59:56 -0700

    created 1-memcpy.c file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * _memcpy - A function that copy's a var data to another
+ * @dest: an input to the function
+ * @src: an input to the function
+ * @n: an input to the function
+ * Return: return a pointer to an input
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned char i = 0;
+	char *p = dest;
+	char r = dest;
+
+	while (i < n)
+	{
+		*p = *(src + i);
+		i++;
+		p++;
+	}
+	return (r);
+}
