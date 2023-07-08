8720f7ab27d85d8e86d6ef8358ae2a964c7e85a6
InfoColony <piusebiama@gmail.com>
2023-07-08 11:32:51 -0700

    created 102-fibonacci.c file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,18 +6,20 @@
  * fibonacci - A function that prints the fibonacci sequence
  * @n: an input to the function
  * Return: return 0
+ * @arr: an input to the function
  */
 
-long int arr[50];
-long int fibonacci(int n);
+
+long int fibonacci(int n, long int *arr);
 
 int main(void)
 {
 	int i = 1;
+	long int arr[50] = {0};
 
 	for (; i <= 50; i++)
 	{
-		printf("%ld", fibonacci(i));
+		printf("%ld", fibonacci(i, arr));
 		if (i < 50)
 			printf(", ");
 	}
@@ -29,17 +31,18 @@ int main(void)
  * fibonacci - A function that prints the fibonacci sequence
  * Return: return a long int
  * @a: an input to the function
+ * @arr: an input to the function
  */
 
-long int fibonacci(int a)
+long int fibonacci(int a, long *arr)
 {
-	if (a <= 1)
+	if (a <= 2)
 	{
 		arr[a] = a;
-		return (1);
+		return (a);
 	}
 	if (arr[a])
 		return (arr[a]);
-	arr[a] = (fibonacci(a - 1) + fibonacci(a - 2));
+	arr[a] = (fibonacci(a - 1, arr) + fibonacci(a - 2, arr));
 	return (arr[a]);
 }
