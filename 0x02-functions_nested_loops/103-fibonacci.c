1e7349d68217b33cb79b3edabae4a44fc4b62e89
InfoColony <piusebiama@gmail.com>
2023-07-08 22:21:25 -0700

    created 103-fibonacci.c file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,15 +2,38 @@
 
 /**
  * main - Entry point of the program
+ * @c: an input
  * Return: always 0
  */
 
+long int fib(int c);
 
 int main(void)
 {
-	int i;
+	int i = 0;
+	long int t;
+	long int maxv = 4000000;
+	long int v = 0;
 
-	for (i = 0; i < 5; i++)
-		putchar('\n');
+	while (v < maxv)
+	{
+		t = fib(i);
+		if (t % 2 == 0)
+			v = v + t;
+		i++;
+	}
+	printf("%ld\n", v);
 	return (0);
+}
+/**
+ * fib - a function that prints fibonacci sequence
+ * @a: an input
+ * Return: return the value of fib
+ */
+
+long int fib(int a)
+{
+	if (a <= 1)
+		return (1);
+	return (fib(a - 1) + fib(a - 2));
 }
