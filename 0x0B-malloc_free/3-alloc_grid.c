ff82af693f832a5ca92b72c495d45e5b713d4da1
InfoColony <piusebiama@gmail.com>
2023-07-10 11:41:30 -0700

    created 3-alloc_grid.c file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - function that create a two dimentional array
+ * @width: An input to the function
+ * @height: An input to the function
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int **tar;
+	int j;
+
+	tar = malloc(width * sizeof(int));
+	if (tar == NULL || width <= 0 || height <= 0)
+		return (NULL);
+	for (; i < width; i++)
+	{
+		tar[i] = malloc(height * sizeof(int));
+		j = 0;
+		for (; j < height; j++)
+			tar[i] = 0;
+	}
+	return (tar);
+}
