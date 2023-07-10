7cd3de070e18f3c2d6417785bbde1e6fa05db3ba
InfoColony <piusebiama@gmail.com>
2023-07-10 11:41:30 -0700

    created 3-alloc_grid.c file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,12 +16,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	tar = malloc(width * sizeof(int *));
+	tar = malloc(width * sizeof(int));
 	if (tar == NULL)
 		return (NULL);
 	for (; i < width; i++)
 	{
-		tar[i] = malloc(height * sizeof(int));
+		tar[i] = malloc(height * sizeof(int *));
 		j = 0;
 		if (tar[i] == NULL)
 		{
