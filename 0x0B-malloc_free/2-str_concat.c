0148978ee0de771cefa41a7a207071af4b011c69
InfoColony <piusebiama@gmail.com>
2023-07-10 07:49:23 -0700

    created 2-str_concat.c file

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - A function that concatinates two strings
+ * @s1: An input to the function
+ * @s2: An input to the function
+ * Return: return the concatinated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *i = s1;
+	char *i2 = s2;
+	int len = 0;
+	char *cst;
+	int j = 0;
+
+	while (*i != '\0')
+	{
+		len++;
+		i++;
+	}
+	while (*i2 != '\0')
+	{
+		len++;
+		i2++;
+	}
+	i = s1;
+	i2 = s2;
+	cst = malloc(len);
+	if (cst == NULL)
+		return (NULL);
+	for (; j < len; j++)
+	{
+		if (*i != '\0')
+		{
+			cst[j] = *i;
+			i++;
+		}
+		if (*i == '\0')
+		{
+			if (*i2 != '\0')
+			{
+				cst[j + 1] = *i2;
+				i2++;
+			}
+		}
+	}
+	cst[j] = '\0';
+	return (cst);
+}
