507a37ace2227356a210a13a50272c811a9c8cd6
InfoColony <piusebiama@gmail.com>
2023-07-11 18:01:51 -0700

    created 101-strtow.c file

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -39,38 +39,38 @@ char **strtow(char *str)
 	st = malloc((hld + 1) * sizeof(char *));
 	if (st == NULL || hld == 0)
 		return (NULL);
-	 while (*i != '\0')
-    {
-        if (*i != 32)
-        {
-            char *t = i;
-            int wl = 0;
-            while(*i != 32)
-            {
-                wl++;
-                i++;
-                if (*i == '\0' || *i == 32)
-                {
-                    int sa = 0;
-                    st[j] = malloc((wl + 1) * sizeof(char));
-                    while(*t != *i)
-                    {
-                        st[j][sa] = *t;
-                        t++;
-                        sa++;
-                        if (*t == *i)
-                        {
-                            st[j][sa] = '\0';
-                            j++;
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-            continue;
-        }
-        i++;
-    }
+	while (*i != '\0')
+	{
+		if (*i != 32)
+		{
+			char *t = i;
+			int wl = 0;
+			while(*i != 32)
+			{
+				wl++;
+				i++;
+				if (*i == '\0' || *i == 32)
+				{
+					int sa = 0;
+					st[j] = malloc((wl + 1) * sizeof(char));
+					while(*t != *i)
+					{
+						st[j][sa] = *t;
+						t++;
+						sa++;
+						if (*t == *i)
+						{
+							st[j][sa] = '\0';
+							j++;
+							break;
+						}
+					}
+					break;
+				}
+			}
+			continue;
+		}
+		i++;
+	}
 	return (st);
 }
