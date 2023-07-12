a2cfdf132a42461f0d11579af42b742b61d2d7c0
InfoColony <piusebiama@gmail.com>
2023-07-12 01:34:57 -0700

    created 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @c: an input to the program
+ * @arg: an input to the program
+ * Return: return 0 on success and 1 on failure
+ */
+
+int main(int c, char *arg[])
+{
+	int j, i = 1;
+	int sum = 0;
+
+	if (c <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (; i < c; i++)
+	{
+		j = atoi(arg[i]);
+		if (j == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += j;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
