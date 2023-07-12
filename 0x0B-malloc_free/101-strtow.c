bc8776a9762b79800e25f46654431cb5ba5711fd
InfoColony <piusebiama@gmail.com>
2023-07-11 18:01:51 -0700

    created 101-strtow.c file

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,6 +6,7 @@
  * @str: An input to the function
  * Return: return the created string array
  */
+void func(char **, char *, int *);
 
 char **strtow(char *str)
 {
@@ -38,38 +39,47 @@ char **strtow(char *str)
 	st = malloc((hld + 1) * sizeof(char *));
 	if (st == NULL || hld == 0)
 		return (NULL);
-	while (*i != '\0')
-	{
-		if (*i != 32)
-		{
-			char *t = i;
-			int wl = 0;
-			while(*i != 32)
-			{
-				wl++;
-				i++;
-				if (*i == '\0' || *i == 32)
-				{
-					int sa = 0;
-					st[j] = malloc((wl + 1) * sizeof(char));
-					while(*t != *i)
-					{
-						st[j][sa] = *t;
-						t++;
-						sa++;
-						if (*t == *i)
-						{
-							st[j][sa] = '\0';
-							j++;
-							break;
-						}
-					}
-					break;
-				}
-			}
-			continue;
-		}
-		i++;
-	}
+	func(st, str, j);
 	return (st);
+}
+
+void func(char **pst, char *pstr, int *pj)
+{
+	char *i = pstr;
+	char **st = pst;
+	int *j = pj;
+
+	 while (*i != '\0')
+    {
+        if (*i != 32)
+        {
+            char *t = i;
+            int wl = 0;
+            while(*i != 32)
+            {
+                wl++;
+                i++;
+                if (*i == '\0' || *i == 32)
+                {
+                    int sa = 0;
+                    st[j] = malloc((wl + 1) * sizeof(char));
+                    while(*t != *i)
+                    {
+                        st[j][sa] = *t;
+                        t++;
+                        sa++;
+                        if (*t == *i)
+                        {
+                            st[j][sa] = '\0';
+                            j++;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            continue;
+        }
+        i++;
+    }
 }
