5daaf2a6debc5bb6ff84a5b1330d34ba5ca87d6f
InfoColony <piusebiama@gmail.com>
2023-07-13 14:23:05 -0700

    created 0-malloc_checked.c and main.h files

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,12 +7,12 @@
  * Return: returned a void pointer on success and 98 on failure
  */
 
-void *malloc_checked (unsigned int b)
+void *malloc_checked(unsigned int b)
 {
 	void *p;
 
 	p = malloc(b);
 	if (p == NULL)
-		exit (98);
+		exit(98);
 	return (p);
 }
