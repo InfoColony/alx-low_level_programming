6cbb2721b1fa08266290ece11317122d547edf1b
InfoColony <piusebiama@gmail.com>
2023-07-15 05:47:24 -0700

    created 6-is_prime_number.c file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,33 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * is_prime_number - A function that chaecks if a number is prime
+ * @int: An input to the function
+ * Return: 0
  */
+
+int is_rec(int, int);
 
 int is_prime_number(int n)
 {
-	int i = 1;
+	int a = n - 1;
 
-	
-	for (; i < 10; i++)
-	{
-		if (i % n == 0)
-			return (0);
-	}
-	return (1);
+	return (is_rec(n, a));
+}
+/**
+ * is_rec - A function that calculates a prime number
+ * @i: an input to the function
+ * @j: an input to the function
+ * Return: 0
+ */
+
+int is_rec(int i, int j)
+{
+	if (i <= 1)
+		return (0);
+	if (j == 1)
+		return (1);
+	if (i % j == 0)
+		return (0);
+	return (is_rec(i, j - 1));
 }
