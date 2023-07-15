4fd2e458fde083648715075708111388627f8f43
InfoColony <piusebiama@gmail.com>
2023-07-15 01:34:30 -0700

    created 4-pow_recursion.c file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int rec(int, int, int);
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (rec(n, 1, n));
+}
+
+int rec(int i,int j,int k)
+{
+	int av = (j + k) / 2;
+
+	if (av * av == i)
+		return (av);
+	if (j == k || j == k - 1)
+		return (-1);
+	if (av * av > i)
+		return (rec(i, j, av -1));
+	return (rec(i, av, k));
+}
