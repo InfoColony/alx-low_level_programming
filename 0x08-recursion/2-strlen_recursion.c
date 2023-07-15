9d131e897243a195087e958598ecefd9b94e4145
InfoColony <piusebiama@gmail.com>
2023-07-15 00:42:54 -0700

    created 2-strlen_recursion.c file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	if (*s == '\0')
+		return (0);
+
+	_strlen_recursion(char s++;);
+	i++;
+	return (i);
+}
