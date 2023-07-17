e8abcaa7b6ee4d783e3330b1b29ce249becbe6b2
InfoColony <piusebiama@gmail.com>
2023-07-17 14:24:09 -0700

    created 1-init_dog.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,12 @@
 #include "dog.h"
-/*
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-*/
+
+/**
+ * init_dog - A void function
+ * @name: An input
+ * @age: An input
+ * @owner: An input
+ * @d: an input
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
