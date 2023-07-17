f688f1363259fac6becdec68a1329884b5ac8fd5
InfoColony <piusebiama@gmail.com>
2023-07-17 14:24:09 -0700

    created 1-init_dog.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,43 +7,9 @@
  * @owner: An input
  * @d: an input
  */
-int len(char *);
-void stc(char *,char *);
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	int j = len(name);
-	d->name = malloc(j + 1);
-	stc(d->name,name);
+	d->name = name;
 	d->age = age;
-	j=len(owner);
-	d->owner = malloc(j + 1);
-	stc(d->owner,owner);
-}
-
-
-int len(char *s)
-{
-	int i = 0;
-	char *p = s;
-
-	
-	while (*p != '\0')
-	{
-		i++;
-		p++;
-	}
-	return (i);
-}
-
-void stc(char *s1, char *s2)
-{
-	char *c = s2;
-	int li = 0;
-	while(*c != '\0')
-	{
-		s1[li] = *c;
-		c++;
-		li++;
-	}
-	s1[li] = '\0';
+	d->owner = owner;
 }
