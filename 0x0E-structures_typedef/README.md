0e079797f17bbb6a708fe995aeabfa35e96a3cfe
InfoColony <piusebiama@gmail.com>
2023-07-17 10:15:22 -0700

    created dog.h file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This is a project on C structures and typedefs at ALX.
+All task files can be found here.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A structure for dog
+ * @name: A member of the struct dog
+ * @age: A struct dog member
+ * @owner: A struct member
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
