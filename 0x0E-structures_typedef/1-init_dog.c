75ef5af96885d508ddfc3fbf7c524549942e9339
InfoColony <piusebiama@gmail.com>
2023-07-17 14:24:09 -0700

    created 1-init_dog.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,12 @@
 #include "dog.h"
-
+/*
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,13 @@
  * @owner: A struct member
  */
 
-/*
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-*/
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_H */
