192abd9b1eb8eb2b1fc51c6e02c639883f7fc19e
InfoColony <piusebiama@gmail.com>
2023-07-17 14:24:09 -0700

    created 1-init_dog.c file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,7 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = malloc(sizeof(owner));
-	if (d-> == NULL)
+	if (d->owner == NULL)
 		return;
 	d->owner = owner;
 	return;
