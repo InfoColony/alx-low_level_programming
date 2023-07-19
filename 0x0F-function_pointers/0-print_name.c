40b0bed8a08bae7fdf8e27131490cf30bf728927
InfoColony <piusebiama@gmail.com>
2023-07-19 16:22:27 -0700

    created 0-print_name.c and main.h files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - A function the prints a string
+ * @name: An input to the function
+ * @f: A pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+This is a project on pointers to function in C at ALX.
+All task files can be found in this directory.
