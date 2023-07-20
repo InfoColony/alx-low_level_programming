cdd3365ff3d4a2415874d42d35deb1a293cf472d
InfoColony <piusebiama@gmail.com>
2023-07-19 17:07:56 -0700

    created 1-array_iterator.c file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	/*if (!array || !action)
 		return;
-		for (; i < s; i++)*/
-		action(array[i]);
+		for (; i < s; i++)
+		action(array[i]);*/
 }
