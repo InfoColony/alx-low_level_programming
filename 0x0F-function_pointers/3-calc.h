d03245fa5c5cfd23ed50d368185913118b81c0ef
InfoColony <piusebiama@gmail.com>
2023-07-19 20:27:00 -0700

    created 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h files

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,17 @@
+#ifndef CALC_H
+#define CALC_H
+#include <stdio.h>
+
+typedef struct
+{
+	char *optn;
+	int (* func)(int,int);
+} op_t;
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif /* CALC_H */
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+
+int main(int cnt, char *arg[])
+{
+	int n1 = atoi(arg[1]);
+	int n2 = atoi(arg[3]);
+	int (*res)(int,int);
+
+	if (cnt != 4)
+	{
+		printf("Error from cnt\n");
+		exit(98);
+	}
+	if (get_op_func(arg[2]) == NULL)
+	{
+		printf("Error from get_op\n");
+		exit(98);
+	}
+	if ((strcmp(arg[2],"/") == 0 || strcmp(arg[2],"%") == 0) && n2 == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	res = get_op_func(arg[2]);
+	printf("%d\n", res(n1,n2));
+	return(0);
+}
